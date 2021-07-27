25086a95c3937d9706c714c8473133fcede8677f
sanam <simian114@gmail.com>
2021-07-27 15:03:25 +0900

    refactor: axios 인스턴스로 분리

--- src/services/apis/comment.js
+++ src/services/apis/comment.js
@@ -1,11 +1,13 @@
-import axios from 'axios';
-
-// NOTE: 인자로 들어온 페이지에 해당하는 걸 가져와야함
+import { axiosInstance } from './axiosInstance';
 
 export const commentAPI = {
   getComments: (page) =>
-    axios({
+    axiosInstance.request({
       method: 'GET',
-      url: `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=10`,
+      url: '/comments',
+      params: {
+        _page: page,
+        _limit: '10',
+      },
     }),
 };
